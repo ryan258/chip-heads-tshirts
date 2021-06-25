df946c6e02d4e5cfcd5ebc72ffa1da814333f01a
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-06-25 11:41:42 -0500

    Update menu-items to navigate withRouter HOC

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -1,22 +1,23 @@
 import React from 'react'
+// withRouter is a HOC - a function that takes a component as an arg, then returns a modified component
+import { withRouter } from 'react-router-dom'
 
 import './menu-item.styles.scss'
 
-const MenuItem = ({ title, imageUrl, size, id, linkUrl }) => {
-  return (
-    <div className={`${size} menu-item`}>
-      <div
-        className="background-image"
-        style={{
-          backgroundImage: `url(${imageUrl})`
-        }}
-      ></div>
-      <div className="content">
-        <h1 className="title">{title.toUpperCase()}</h1>
-        <span className="subtitle">SHOP NOW</span>
-      </div>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+    <div
+      className="background-image"
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    ></div>
+    <div className="content">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <span className="subtitle">SHOP NOW</span>
     </div>
-  )
-}
+  </div>
+)
 
-export default MenuItem
+// now we get a super powered MenuItem component of the same name w/ access to location, match, and history props
+export default withRouter(MenuItem)
