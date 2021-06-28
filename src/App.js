836218ed3fb89126506fa29a98cd21d9df3814c0
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-06-28 08:35:56 -0500

    Add header component to all pages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import './App.css'
+
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component.jsx'
+
+import Header from './components/header/header.component'
 
 function App() {
   return (
     <div>
+      <Header />
       {/* Switch, when a path is matched, nothing after that is rendered */}
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
