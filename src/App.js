0b9825113a7fa102d919d2d150f62f709c50f600
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-06-30 16:29:33 -0500

    Finish sign in handle submit

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ class App extends React.Component {
             // console.log(this.state)
             // }
           )
-          console.log(this.state)
+          // console.log(this.state)
         })
         // console.log(this.state)
       } else {
